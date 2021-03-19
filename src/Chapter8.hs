9880a5d8727fe0e9b6a7f271946cafa492f9382a
Frank Jung <frankhjung@linux.com>
2021-03-19 15:08:17 +1100

    simplify minmax

--- src/Chapter8.hs
+++ src/Chapter8.hs
@@ -8,6 +8,5 @@ readInts :: String -> [Int]
 readInts s = map read $ words s
 
 minMax :: Ord a => [a] -> Maybe (a,a)
-minMax (h:t) = Just $ foldr (\x (low, high) -> (if x < low then x else low, if x > high then x else high)) (h,h) t
-minMax _ = Nothing -- no high or low, ie constant
-
+minMax (h:t) = Just $ foldr (\x (low, high) -> (min x low, max x high)) (h,h) t
+minMax _     = Nothing -- no high or low, ie constant
